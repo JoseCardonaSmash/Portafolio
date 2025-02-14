c73aa092c0cd15c8ea2f304994e058aacaa74159
JoseCardonaSmash <luiscardonadevelop@gmail.com>
2025-02-14 15:39:21 -0500

    Se ajsuta el erroro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
  
 
 // https://astro.build/config
@@ -15,5 +15,5 @@ export default defineConfig({
   ],
   vite: { plugins: [tailwindcss()] },
   output: 'server',
-  adapter: vercel({}),
+  adapter: vercel(),
 });
